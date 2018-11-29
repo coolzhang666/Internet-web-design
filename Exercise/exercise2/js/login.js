7205f69146a74ca6d47acb74ac80eb52ba883f4e
coolzhang666 <2227603597@qq.com>
2018-11-29 15:07:29 +0800

    修改登录界面样式表，优化header

--- Exercise/exercise2/js/login.js
+++ Exercise/exercise2/js/login.js
@@ -1,22 +1,15 @@
 window.onload = function() {
     var span1 = document.getElementById("span1");
     var span2 = document.getElementById("span2");
-    span1.addEventListener('click', change1, false);
-    span2.addEventListener('click', change2, false);
+    var x = document.getElementById("go_signin");
+    var y = document.getElementById("go_signup");
+    x.addEventListener('click', change1, false);
+    y.addEventListener('click', change2, false);
+    span1.addEventListener('click', change2, false);
+    span2.addEventListener('click', change1, false);
 }
 
 function change1() {
-    var signup = document.getElementById("signup");
-    var signin = document.getElementById("signin");
-    var form1 = document.getElementById("form1");
-    var form2 = document.getElementById("form2");
-    signup.style.backgroundColor = "rgba(240, 240, 240, 0)";
-    signin.style.backgroundColor = "rgba(160, 160, 160, 0.6)";
-    form1.style.cssText = "display: block";
-    form2.style.cssText = "display: none";
-}
-
-function change2() {
     var signup = document.getElementById("signup");
     var signin = document.getElementById("signin");
     var form1 = document.getElementById("form1");
@@ -25,4 +18,15 @@ function change2() {
     signin.style.backgroundColor = "rgba(240, 240, 240, 0)";
     form1.style.cssText = "display: none";
     form2.style.cssText = "display: block";
+}
+
+function change2() {
+    var signup = document.getElementById("signup");
+    var signin = document.getElementById("signin");
+    var form1 = document.getElementById("form1");
+    var form2 = document.getElementById("form2");
+    signup.style.backgroundColor = "rgba(240, 240, 240, 0)";
+    signin.style.backgroundColor = "rgba(160, 160, 160, 0.6)";
+    form1.style.cssText = "display: block";
+    form2.style.cssText = "display: none";
 }
